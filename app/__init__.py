67c0276c6397b374b99897c08efdb529ef5b06a0
Jason Mize <jasonemize@gmail.com>
2017-05-31 09:53:09 -0400

    working towards sass implementation

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask.ext.scss import Scss
 
 app = Flask(__name__)
+Scss(app, static_dir='static', asset_dir='assets')
 
 from app import views
 from app import filters
+
