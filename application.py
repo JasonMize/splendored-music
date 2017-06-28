5cc63cac4f88573b14e27e2942727766f0d909aa
Jason Mize <jasonemize@gmail.com>
2017-06-28 12:17:50 -0400

    tweak

--- application.py
+++ application.py
@@ -1,13 +1,11 @@
 import sys
  
 from app import app
- 
+# from flask import Flask 
 
 # app = Flask(__name__)
-
-
-# if __name__ == "__main__":
+if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
     # application.debug = True
-app.run()
+    app.run()
