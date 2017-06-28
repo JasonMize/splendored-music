d83c7eb6d3de0369e9f2bb97002fcbf0607ae945
Jason Mize <jasonemize@gmail.com>
2017-06-28 10:14:15 -0400

    fixed urls

--- application.py
+++ application.py
@@ -1,13 +1,13 @@
 import sys
  
-from flask import Flask
+from app import app
  
 
-application = Flask(__name__)
+# app = Flask(__name__)
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
     # application.debug = True
-    application.run()
+app.run()
