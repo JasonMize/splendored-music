597978e1c543dad697ecfc6a6ea67a4c16108d7d
Jason Mize <jasonemize@gmail.com>
2017-06-29 12:25:27 -0400

    new path names

--- application.py
+++ application.py
@@ -1,9 +1,9 @@
 from flask import * 
 
-application = Flask(__name__)
-app = application
 
-from app import app
+# app = application
+
+from app import app as application
  
 if __name__ == "__main__":
-    app.run()
+    application.run(debug=True)
