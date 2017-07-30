d28fb0f59a8d156a2b747581775c2a2423eaed02
Jason Mize <jasonemize@gmail.com>
2017-07-30 16:56:04 -0400

    images for songs

---
+++ app/static/main.js
@@ -0,0 +1,23 @@
+
+
+$(document).ready(function() {
+
+    // initial page size
+    let browserWidth = $(window).width();
+    let percentFromTop = 0.45
+    let desiredMargin = browserWidth * percentFromTop;
+    console.log('Starting WIDTH: ', browserWidth);
+    console.log('Starting MARGIN: ', desiredMargin);
+
+    // as page size changes
+    $(window).on('resize', function(){
+        if($(this).width() != browserWidth){
+            browserWidth = $(this).width();
+            desiredMargin = $(this).width() * percentFromTop;
+            console.log('WIDTH: ', browserWidth);
+            console.log('MARGIN: ', desiredMargin);
+            location.reload();
+        }
+    });
+    $('#logo_spacing').css('margin-top', desiredMargin);
+});
