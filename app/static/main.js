ae5fa728f18088076ef4b4a3bf4bd3a60b8a39d8
Jason Mize <jasonemize@gmail.com>
2017-08-07 23:14:41 -0400

    starting and stopping audio

--- app/static/main.js
+++ app/static/main.js
@@ -6,16 +6,16 @@ $(document).ready(function() {
     let browserWidth = $(window).width();
     let percentFromTop = 0.45
     let desiredMargin = browserWidth * percentFromTop;
-    console.log('Starting WIDTH: ', browserWidth);
-    console.log('Starting MARGIN: ', desiredMargin);
+    // console.log('Starting WIDTH: ', browserWidth);
+    // console.log('Starting MARGIN: ', desiredMargin);
 
     // as page size changes
     $(window).on('resize', function(){
         if($(this).width() != browserWidth){
             browserWidth = $(this).width();
             desiredMargin = $(this).width() * percentFromTop;
-            console.log('WIDTH: ', browserWidth);
-            console.log('MARGIN: ', desiredMargin);
+            // console.log('WIDTH: ', browserWidth);
+            // console.log('MARGIN: ', desiredMargin);
             location.reload();
         }
     });
